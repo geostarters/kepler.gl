516281a49584f9f9bbb898a6b28a688dacbc93fe
sfggeogis <sfggeogis@gmail.com>
2021-06-03 09:44:54 +0200

    add

--- src/connect/keplergl-connect.js
+++ src/connect/keplergl-connect.js
@@ -18,35 +18,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-<<<<<<< HEAD
-import {connect as reduxConnect} from 'react-redux';
-import withLocalSelector from './with-local-selector';
-
-const defaultMapStateToProps = state => state;
-const defaultMapDispatchToProps = () => dispatch => ({dispatch});
-
-export const connect = (
-  mapStateToProps = defaultMapStateToProps,
-  makeMapDispatchToProps = defaultMapDispatchToProps,
-  reduxMergeProps,
-  options
-) => BaseComponent => {
-  const mapDispatchToProps = makeMapDispatchToProps();
-  const reduxMapState = (state, props) => mapStateToProps(props.selector(state), props, state);
-
-  const reduxMapDispatch = (dispatch, props) => mapDispatchToProps(props.dispatch, props, dispatch);
-
-  const ReduxComponent = reduxConnect(
-    reduxMapState,
-    reduxMapDispatch,
-    reduxMergeProps,
-    options
-  )(BaseComponent);
-
-  // save selector to context so it can be accessed by its children
-  return withLocalSelector(ReduxComponent);
-};
-=======
 import {connect as reduxConnect} from 'react-redux';
 import withLocalSelector from './with-local-selector';
 
@@ -74,4 +45,3 @@ export const connect = (
   // save selector to context so it can be accessed by its children
   return withLocalSelector(ReduxComponent);
 };
->>>>>>> master
--- src/connect/with-local-selector.js
+++ src/connect/with-local-selector.js
@@ -18,53 +18,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-<<<<<<< HEAD
-import React, {Component} from 'react';
-import {createSelector} from 'reselect';
-import KeplerGlContext from 'components/context';
-
-const identity = state => state;
-
-const mergeSelectors = (parentSelector, childSelector) => state =>
-  childSelector(parentSelector(state));
-
-// store the parent selector in the parent context
-// and return the parent component
-// when a selector is passed to a container component,
-// it will be stored in the context and passed down to child components,
-// as well as prop to the given component
-const withLocalSelector = ParentComponent => {
-  class WithConnectSelector extends Component {
-    static contextType = KeplerGlContext;
-
-    selectorFromContext = (_, ctx) => (ctx.selector ? ctx.selector : identity);
-    selectorFromProps = (props, _) => (props.selector ? props.selector : identity);
-    idFromProps = (props, _) => props.id;
-    computedSelector = createSelector(
-      this.selectorFromContext,
-      this.selectorFromProps,
-      (ctx, props) => mergeSelectors(ctx, props)
-    );
-    contextSelector = createSelector(this.computedSelector, this.idFromProps, (selector, id) => ({
-      selector,
-      id
-    }));
-
-    render() {
-      const computedContext = this.contextSelector(this.props, this.context);
-      return (
-        <KeplerGlContext.Provider value={computedContext}>
-          <ParentComponent {...this.props} selector={computedContext.selector} />
-        </KeplerGlContext.Provider>
-      );
-    }
-  }
-
-  return WithConnectSelector;
-};
-
-export default withLocalSelector;
-=======
 import React, {Component} from 'react';
 import {createSelector} from 'reselect';
 import KeplerGlContext from 'components/context';
@@ -112,4 +65,3 @@ const withLocalSelector = ParentComponent => {
 };
 
 export default withLocalSelector;
->>>>>>> master
